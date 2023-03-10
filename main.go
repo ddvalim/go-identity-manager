012a0e7ae01e4256b2f1d5ec83a8dec5c7426130
Diovana Valim <diovanavalimm@gmail.com>
2023-03-10 20:45:20 -0300

    feat: add main func

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"go-identity-manager/cmd/router"
+	"log"
+	"net/http"
+)
+
+func main() {
+	routes := router.CreateRouter()
+
+	fmt.Printf("Server running on port %d\n", 9696)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", 9696), routes))
+}
